76a7ea14403d15fc521666595bd89b36bf627804
karoko <karoko.jnr99@gmail.com>
2024-07-22 15:58:00 +0300

    Added PostgreSQL database configuration and connection handling

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/karokojnr/GoBuzz/internal/db"
 	"github.com/karokojnr/GoBuzz/internal/env"
 	"github.com/karokojnr/GoBuzz/internal/store"
 )
@@ -10,9 +11,30 @@ import (
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:changeme@127.0.0.1:5432/gobuzz?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+
+	log.Println("successfully connected to the database🚀")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
