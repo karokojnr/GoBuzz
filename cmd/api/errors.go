2b7787e8d3da7f4e17630605997e479db16ff8dc
karoko <karoko.jnr99@gmail.com>
2024-07-23 12:47:59 +0300

    Refactored error handling in API endpoints :recycle:

---
+++ cmd/api/errors.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("Internal server error: %s path: %s error: %s", r.Method, r.URL.Path, err.Error())
+	writeJSONError(w, http.StatusInternalServerError, "the server encountered a problem")
+}
+
+func (app *application) badRequestRepsonse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("Bad request error: %s path: %s error: %s", r.Method, r.URL.Path, err.Error())
+	writeJSONError(w, http.StatusBadRequest, err.Error())
+}
+
+func (app *application) notFoundError(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("Not found error: %s path: %s error: %s", r.Method, r.URL.Path, err.Error())
+	writeJSONError(w, http.StatusNotFound, "not found")
+}
