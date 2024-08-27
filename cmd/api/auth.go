616f533151ca2c9efbb17cf1bd902897ae924b8e
karoko <karoko.jnr99@gmail.com>
2024-08-27 23:01:58 +0300

    Added hashing functionality for password

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/karokojnr/GoBuzz/internal/store"
+)
 
 type RegisterUserPayload struct {
 	Username string `json:"username" validate:"required,max=100"`
@@ -21,4 +25,30 @@ type RegisterUserPayload struct {
 //	@Failure		500		{object}	error
 //	@Router			/authentication/user [post]
 func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
+	var payload RegisterUserPayload
+	if err := readJSON(w, r, &payload); err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(payload); err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
+
+	user := &store.User{
+		Username: payload.Username,
+		Email:    payload.Email,
+	}
+
+	if err := user.Password.Set(payload.Password); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+
+	// todo: change after adding authentication
+	if err := app.jsonResponse(w, http.StatusCreated, nil); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
 }
--- internal/store/users.go
+++ internal/store/users.go
@@ -3,6 +3,8 @@ package store
 import (
 	"context"
 	"database/sql"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 // model
@@ -10,8 +12,25 @@ type User struct {
 	ID        int64  `json:"id"`
 	Username  string `json:"username"`
 	Email     string `json:"email"`
-	Password  string `json:"-"`
+	Password  password `json:"-"`
 	CreatedAt string `json:"created_at"`
+}
+
+type password struct {
+	text *string
+	hash []byte
+}
+
+func (p *password) Set(text string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(text), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	p.text = &text
+	p.hash = hash
+
+	return nil
 }
 
 type UserStore struct {
