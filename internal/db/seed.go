05aca4c9e3b7dc30093d1144c931934cb0bd2b16
karoko <karoko.jnr99@gmail.com>
2024-08-29 16:08:30 +0300

    feat: Update db.Seed function to accept a database connection
    The `db.Seed` function in `cmd/migrate/seed/main.go` has been updated to accept a database connection as an argument. This change allows for better control and management of the database connection within the function.

--- internal/db/seed.go
+++ internal/db/seed.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"math/rand"
@@ -94,17 +95,20 @@ var comments = []string{
 	"Advanced Go techniques were well explained. Great article!",
 }
 
-func Seed(store store.Storage) {
+func Seed(store store.Storage, db *sql.DB) {
 
 	ctx := context.Background()
 
 	users := generateUsers(100)
+	tx, _ := db.BeginTx(ctx, nil)
 	for _, user := range users {
-		if err := store.Users.Create(ctx, user); err != nil {
+		if err := store.Users.Create(ctx, tx, user); err != nil {
+			_ = tx.Rollback()
 			log.Println("Error creating user: ", err)
 			return
 		}
 	}
+	tx.Commit()
 
 	posts := generatePosts(200, users)
 	for _, post := range posts {
@@ -131,7 +135,6 @@ func generateUsers(n int) []*store.User {
 		users[i] = &store.User{
 			Username: usernames[i%len(usernames)] + fmt.Sprintf("%d", i),
 			Email:    usernames[i%len(usernames)] + fmt.Sprintf("%d", i) + "@example.com",
-			Password: "changeme",
 		}
 	}
 	return users
