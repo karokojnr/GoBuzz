64542b520be6f0a56331036ad4fad366066f1f6b
karoko <karoko.jnr99@gmail.com>
2024-09-09 23:35:37 +0300

    Refactor mailer package and implement Sendgrid integration

--- internal/mailer/mailer.go
+++ internal/mailer/mailer.go
@@ -1,5 +1,10 @@
 package mailer
 
+const (
+	FromName   = "GoBuzz"
+	maxRetries = 3
+)
+
 type Client interface {
 	Send(templateFile, username, email string, data any, isSandbox bool) error
 }
--- internal/mailer/sendgrid.go
+++ internal/mailer/sendgrid.go
@@ -1,6 +1,14 @@
 package mailer
 
-import "github.com/sendgrid/sendgrid-go"
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+)
 
 type SendgridMailer struct {
 	fromEmail string
@@ -15,4 +23,35 @@ func NewSendgrid(fromEmail, apiKey string) *SendgridMailer {
 		apiKey:    apiKey,
 		client:    client,
 	}
+}
+
+func (s *SendgridMailer) Send(templateFile, username, email string, data any, isSandbox bool) error {
+	from := mail.NewEmail(FromName, s.fromEmail)
+	to := mail.NewEmail(username, email)
+
+	subject := new(bytes.Buffer)
+	body := new(bytes.Buffer)
+
+	message := mail.NewSingleEmail(from, subject.String(), to, "", body.String())
+	message.SetMailSettings(&mail.MailSettings{
+		SandboxMode: &mail.Setting{
+			Enable: &isSandbox,
+		},
+	})
+
+	for i := 0; i < maxRetries; i++ {
+		res, err := s.client.Send(message)
+		if err != nil {
+			log.Printf("Failed to send email to %v, attempt %d of %d \n", email, i+1, maxRetries)
+			log.Printf("Error: %v \n", err)
+
+			// exponential backoff
+			time.Sleep(time.Second * time.Duration(i+1))
+			continue
+		}
+
+		log.Printf("Email sent with status code: %v \n", res.StatusCode)
+		return nil
+	}
+	return fmt.Errorf("Failed to send email to %v after %d attempts", email, maxRetries)
 }
