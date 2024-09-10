12e527be6969cee418edfd299ff415d1479913af
karoko <karoko.jnr99@gmail.com>
2024-09-10 13:14:54 +0300

    Set up web :tada:

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/v1"
+
+function App() {
+  return (
+    <>
+      <div>App home screen</div>
+    </>
+  )
+}
+
+export default App
