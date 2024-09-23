3013fe2046fe34ab57714430c53fb436c3fc67c2
karoko <karoko.jnr99@gmail.com>
2024-09-23 22:40:38 +0300

    Refactor AuthTokenMiddleware to accept next http.Handler as argument

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -47,45 +47,44 @@ func (app *application) BasicAuthMiddleware() func(http.Handler) http.Handler {
 	}
 }
 
-func (app *application) AuthTokenMiddleware() func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			authHeader := r.Header.Get("Authorization")
-			if authHeader == "" {
-				app.unauthorizedError(w, r, fmt.Errorf("authorization header is missing"))
-				return
-			}
+func (app *application) AuthTokenMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			app.unauthorizedError(w, r, fmt.Errorf("authorization header is missing"))
+			return
+		}
 
-			parts := strings.Split(authHeader, " ")
-			if len(parts) != 2 || parts[0] != "Bearer" {
-				app.unauthorizedBasicError(w, r, fmt.Errorf("authorization header is invalid"))
-				return
-			}
+		parts := strings.Split(authHeader, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			app.unauthorizedBasicError(w, r, fmt.Errorf("authorization header is invalid"))
+			return
+		}
 
-			token := parts[1]
-			jwtToken, err := app.authenticator.ValidateToken(token)
-			if err != nil {
-				app.unauthorizedError(w, r, err)
-				return
-			}
+		token := parts[1]
+		jwtToken, err := app.authenticator.ValidateToken(token)
+		if err != nil {
+			app.unauthorizedError(w, r, err)
+			return
+		}
 
-			claims, _ := jwtToken.Claims.(jwt.MapClaims)
-			userID, err := strconv.ParseInt(fmt.Sprintf("%.f", claims["sub"]), 10, 64)
-			if err != nil {
-				app.unauthorizedError(w, r, err)
-				return
-			}
+		claims, _ := jwtToken.Claims.(jwt.MapClaims)
+		userID, err := strconv.ParseInt(fmt.Sprintf("%.f", claims["sub"]), 10, 64)
+		if err != nil {
+			app.unauthorizedError(w, r, err)
+			return
+		}
 
-			ctx := r.Context()
+		ctx := r.Context()
 
-			user, err := app.store.Users.GetByID(ctx, userID)
-			if err != nil {
-				app.unauthorizedError(w, r, err)
-				return
-			}
+		user, err := app.store.Users.GetByID(ctx, userID)
+		if err != nil {
+			app.unauthorizedError(w, r, err)
+			return
+		}
 
-			ctx = context.WithValue(ctx, userCtx, user)
-			next.ServeHTTP(w, r.WithContext(ctx))
-		})
-	}
+		ctx = context.WithValue(ctx, userCtx, user)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+
 }
