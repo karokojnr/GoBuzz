b3e2be4145136f61cb6c66cbd4cbbda5506babe0
karoko <karoko.jnr99@gmail.com>
2024-10-02 23:25:52 +0300

    Refactor updatePostHandler to use separate method for updating post and deleting user cache :recycle:

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -142,10 +142,10 @@ func (app *application) updatePostHandler(w http.ResponseWriter, r *http.Request
 		post.Content = *payload.Content
 	}
 
-	if err := app.store.Posts.Update(r.Context(), post); err != nil {
-		// todo: return 409 if version mismatch
+	ctx := r.Context()
+
+	if err := app.updatePost(ctx, post); err != nil {
 		app.internalServerError(w, r, err)
-		return
 	}
 
 	if err := app.jsonResponse(w, http.StatusOK, post); err != nil {
@@ -220,4 +220,12 @@ func (app *application) postsContextMiddleware(next http.Handler) http.Handler {
 func getPostFromCtx(r *http.Request) *store.Post {
 	post, _ := r.Context().Value(postCtxKey).(*store.Post)
 	return post
+}
+
+func (app *application) updatePost(ctx context.Context, post *store.Post) error {
+	if err := app.store.Posts.Update(ctx, post); err != nil {
+		return err
+	}
+	app.cacheStore.Users.Delete(ctx, post.ID)
+	return nil
 }
